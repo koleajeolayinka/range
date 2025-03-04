d513841e7ef3f8429900d2ba972cbbf0be2185f2
koleajeolayinka <koleajeolayinka@gmail.com>
2025-03-04 10:27:20 +0100

    feat: refactor application call to action

--- src/components/applicationCallToAction/Index.tsx
+++ src/components/applicationCallToAction/Index.tsx
@@ -1,18 +1,21 @@
 'use client';
 
-import { FC } from 'react';
-import { FaUser, FaUsers } from 'react-icons/fa';
+import {FC} from 'react';
+import {FaUser, FaUsers} from 'react-icons/fa';
 import ApplyCard from "@/components/ApplyCard/Index";
-
-
-
 
 const ApplicationCallToAction: FC = () => {
     return (
-        <div className="flex justify-center items-center gap-8 p-10 bg-[#EBF3FF]">
-            <ApplyCard type="Individual" description="Apply as an individual" icon={<FaUser />} />
-            <ApplyCard type="Team" description="Apply as a team" icon={<FaUsers />} />
-        </div>
+        <section>
+            <div className="flex justify-center items-center gap-8 p-10 bg-[#EBF3FF]">
+                <ApplyCard type="Individual" description="Apply as an individual" icon={<FaUser/>}/>
+                <ApplyCard type="Team" description="Apply as a team" icon={<FaUsers/>}/>
+            </div>
+            <div className={'mt-6 grid place-content-center'}>
+                <h1>We will inform applicants if they were selected by <span className={'font-semibold'}>Monday, March 17, 2025.</span>
+                </h1>
+            </div>
+        </section>
     );
 };
 
